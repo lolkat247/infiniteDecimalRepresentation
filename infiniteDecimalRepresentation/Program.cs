f7a633365f1ef4a8dcbf4b9bb4301e95b0c62e50
Jacob Iglesias <jacob.ds.iglesias.5@gmail.com>
2020-06-04 20:46:06 -0400

    Add project files.

---
+++ infiniteDecimalRepresentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace infiniteDecimalRepresentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
